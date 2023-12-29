651e91c9c225d3228779cf74d400c7698d3e16e1
graietay <sayadon1984@gmail.com>
2023-12-29 09:31:35 +0100

    checkpointapi v1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started with Create React App
+## Available Scripts
+### `npm start`
+#### Utilisation axios pour obtenir des données de  l'API jsonplaceholder à l'intérieur des hooks useEffect et mapper le state listOfUser pour afficher la liste
+
---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import axios from 'axios';
+import './App.css';
+import { useEffect, useState } from 'react';
+import UserList from './UserList';
+
+
+
+
+function App() {
+
+ 
+  return (
+    <div>
+    <UserList></UserList>
+    </div> )
+}
+
+export default App;
